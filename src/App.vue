e51bda6907e4634e621d6ef5e1b813b3e0c06b1d
Robert Banaszak <robert.banaszak1997@gmail.com>
2019-03-15 20:57:01 +0100

    init vue project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Nav></Nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/Nav.vue';
+
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
+
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Raleway:400,700");
+html {
+  font-family: "Raleway", sans-serif;
+}
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+    <nav>
+      <a href="/">
+        <h3>Blog</h3>
+      </a>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <nav>
+      <router-link to="/login">Login</router-link>
+      <router-link to="/register">Register</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+header {
+  padding: $spacing;
+  background-color: $accent;
+  font-size: 1.25rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: $white;
+
+  h3 {
+    margin: 0 $spacing * 2 0 0;
+    display: inline-block;
+  }
+
+  nav a {
+    color: inherit;
+    text-decoration: none;
+    margin-right: $spacing/2;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
